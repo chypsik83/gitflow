f6728b49c2ea11381452dbbf204b2546f03eea60
chypsik83 <shalyapina-dina@mail.ru>
2024-02-19 22:55:03 +0300

    Добавили  функцию get_first_title_cha и docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def get_upper_words(word):
+    """Возвращает слово заглавными буквами"""
     return word.upper()
+
+
+def get_first_title_char(word):
+    """"Возвращает слово заглавной первой буквой"""
+    return word.title()
